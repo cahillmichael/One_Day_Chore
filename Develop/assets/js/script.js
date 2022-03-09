1ea7a9269a2a846e1724f332d10fa9cbb7f3f5df
michaelc <michaelc@carimus.com>
2022-03-09 02:32:37 -0500

    readme and text style changes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,9 @@
-//display current date
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+//display current date, assign by id
+$("#currentDay").text();
 
 //color textarea based on time
+//iterate from 9 to 17 (hours in work day)
+//compare current time to each hour and assign appropriate style
 for(let i = 9; i < 18; i++) {
     if(moment().hour() === i){
         $("#" + i).addClass("present");
@@ -15,13 +17,22 @@ for(let i = 9; i < 18; i++) {
 };
 
 //save text
+//on save button click
 $(".saveBtn").on("click", function(){
-    var hourRow = $(this).closest(".row").find("textarea").attr("id");
-    var taskText = $("#" + hourRow).val();
-    localStorage.setItem(hourRow, taskText);
+
+    // identify the id of the textarea in the same row
+    var rowId = $(this).closest(".row").find("textarea").attr("id");
+
+    // grab text from textarea with that id
+    var taskText = $("#" + rowId).val();
+
+    //write text and id to localStorage
+    localStorage.setItem(rowId, taskText);
 });
 
 //load text
+//iterate from 9 to 17
+//assign localStorage data with corresponding key to textarea with that id
 for(let i = 9; i < 18; i++) {
     $("#" + i).val(localStorage.getItem(i));
 };
