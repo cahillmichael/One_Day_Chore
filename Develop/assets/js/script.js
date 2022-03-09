67792c5dce4dec0d5028691c3613b5b85641a45c
michaelc <michaelc@carimus.com>
2022-03-09 01:22:32 -0500

    color textarea by time

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,19 @@
-console.log(moment().hour());
-console.log(moment());
+
 
 //display current date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+//color textarea based on time
+for(let i = 9; i < 18; i++) {
+    console.log(moment().hour())
+    if(moment().hour() === i){
+        $("#" + i).addClass("present");
+    }
+    else if(moment().hour() < i){
+        $("#" + i).addClass("future");
+
+    }
+    else if(moment().hour() > i){
+        $("#" + i).addClass("past");
+    }
+}
